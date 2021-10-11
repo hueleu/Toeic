19c76d959762b1ac964edd03a2b976b2beaec229
leuhue99 <leuhue.bkhn99@gmail.com>
2021-10-11 07:33:51 +0700

    update

--- app/src/main/java/com/example/toeic/MainActivity.java
+++ app/src/main/java/com/example/toeic/MainActivity.java
@@ -1,39 +1,131 @@
 package com.example.toeic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.example.toeic.controller.AppController;
-import com.example.toeic.data.AnswerListAsyncResponse;
 import com.example.toeic.data.Repository;
+import com.example.toeic.databinding.ActivityMainBinding;
 import com.example.toeic.model.Question;
+import com.google.android.material.snackbar.Snackbar;
 
-import org.json.JSONArray;
-
-import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private ActivityMainBinding binding;
+    private int currentQuestionIndex = 1;
+    List<Question> questionsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        new Repository().getQuestions();
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
         Log.d("hue.leu", "start");
-        List<Question> questions = new Repository().getQuestions(new AnswerListAsyncResponse() {
-            @Override
-            public void processFinished(ArrayList<Question> questionArrayList) {
-                Log.d("hue.leu", "list: "+questionArrayList);
-            }
+        questionsList = new Repository().getQuestions(questionArrayList -> {
+            binding.questionTextview.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
+            binding.textViewOutOf.setText("Question: " +currentQuestionIndex +"/"+questionsList.size());
         });
 
+        binding.buttonNext.setOnClickListener(this::onClick);
+        binding.buttonTrue.setOnClickListener(this::onClick);
+        binding.buttonFalse.setOnClickListener(this::onClick);
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.button_false:
+                checkAnswer(false);
+                updateQuestion();
+                break;
+            case R.id.button_true:
+                checkAnswer(true);
+                updateQuestion();
+                break;
+            case R.id.button_next:
+                currentQuestionIndex = (currentQuestionIndex +1)%questionsList.size();
+                updateQuestion();
+                break;
+        }
+    }
+
+    private void checkAnswer(boolean b) {
+        boolean answer = questionsList.get(currentQuestionIndex).isAnswerTrue();
+        int snackMessageId;
+        if(b == answer){
+            snackMessageId = R.string.correct_answer;
+            fadeAnimation();
+        } else {
+            snackMessageId = R.string.incorrect_answer;
+            shakeAnimation();
+        }
+       Snackbar.make(binding.cardView, snackMessageId, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void updateQuestion() {
+        String question = questionsList.get(currentQuestionIndex).getAnswer();
+        binding.questionTextview.setText(question);
+        binding.textViewOutOf.setText("Question: " +currentQuestionIndex +"/"+questionsList.size());
+    }
+
+    private void fadeAnimation(){
+        AlphaAnimation animation = new AlphaAnimation(1.0f, 0.0f);
+        animation.setDuration(300);
+        animation.setRepeatCount(1);
+        animation.setRepeatMode(Animation.REVERSE);
+
+        binding.cardView.setAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                binding.questionTextview.setTextColor(Color.GREEN);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                binding.questionTextview.setTextColor(Color.WHITE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    private void shakeAnimation(){
+        Animation shake = AnimationUtils.loadAnimation(MainActivity.this, R.anim.shake_animation);
+        binding.cardView.setAnimation(shake);
+        shake.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                binding.questionTextview.setTextColor(Color.RED);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                binding.questionTextview.setTextColor(Color.WHITE);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
